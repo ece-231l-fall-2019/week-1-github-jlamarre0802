16878f23b90f9789c63da9f55021df4fe6d32e96
Jack LaMarre <jlamarre0802@gmail.com>
2019-08-28 18:45:59 -0600

    Week 1

--- hello.cpp
+++ hello.cpp
@@ -3,7 +3,7 @@
 int main(){
 	
 	std::cout <<"My name is Jack LaMarre, I am from Oswego, IL. I want to further expand my knowledge of the coding lanaguage. In the future I plan on either being a Pro Esports coach and/or creating my own company." << std::endl;
-	std::cout << "[Coaching](league.jpg)" <<std::endl;
+	std::cout << "![Coaching](league.jpg)" <<std::endl;
 	return 0;
 }
 
